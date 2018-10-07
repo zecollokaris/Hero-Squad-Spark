6d8229445dc971c29e60b637e2eb3b1da4914dc8
zecollokaris <collo.kariss@gmail.com>
2018-10-07 17:46:57 +0300

    imported hashmaps,maps,modleview & velocity template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,10 @@
 import static spark.Spark.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
 public class App {
     public static void main(String[] args) {
         get("/", (request, response) -> "Hello Friend!");
