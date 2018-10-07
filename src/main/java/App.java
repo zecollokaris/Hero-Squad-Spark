cb3c4666e7511e174e4d0ce090584c4be27119b4
zecollokaris <collo.kariss@gmail.com>
2018-10-07 18:07:28 +0300

    added routefor form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,5 +17,11 @@ public class App {
             model.put("template", "templates/home.vtl" );
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/form.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
     }
 }
