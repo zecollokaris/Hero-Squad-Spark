30891a57e626c27332a5965d110c679d12b3e1ca
zecollokaris <collo.kariss@gmail.com>
2018-10-07 21:04:34 +0300

    changed routes for adding squad

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,9 +18,9 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
-        get("/form", (request, response) -> {
+        get("/squadform", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            model.put("template", "templates/form.vtl");
+            model.put("template", "templates/squadform.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
