bd2404cd1f282ae1968c9db7c02730d31f208925
zecollokaris <collo.kariss@gmail.com>
2018-10-10 16:43:08 +0300

    started deployment

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,16 @@ import spark.template.velocity.VelocityTemplateEngine;
 public class App {
     public static void main(String[] args) {
 
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        setPort(port);
+
 
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
